4c931bbf9de6d09e063b8e17a387860eb65ebc11
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-06-24 10:59:43 +0300

    added dropdown function for droping menus on smaller screens

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const links= document.querySelector(".links");
-const dropdown=document.querySelector(".btn-drop");
+//const dropdown=document.querySelector("#dropit");
 const year=document.querySelector("#year");
 const date=new Date();
 
 const fullyear=date.getFullYear();
 
+
+function dropdown(){
+    links.classList.toggle("hidden")
+}
 year.innerHTML=fullyear;
