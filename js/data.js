1e65a4d68b962d4d87118fa548859763d58fa5b8
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-06-25 20:51:20 -0700

    added for dynamically storing data for us on backend and frontend section, which can be lately upgraded easily

---
+++ js/data.js
@@ -0,0 +1,22 @@
+const backend_data=[
+    {
+        name:"PHP",
+        img:"icons8-php-480.png",
+        description:"As a PHP developer, I am dedicated to delivering exceptional results. With a deep understanding of PHP programming language, I can create dynamic and interactive web applications. I am proficient in various PHP frameworks and libraries, allowing me to build efficient and maintainable codebases",
+    },
+    {
+        name:"NodeJs",
+        img:"icons8-node-js-240.png",
+        description:"As a Node.js developer, I am passionate about building scalable and high-performance applications. With expertise in JavaScript and the Node.js runtime environment, I can develop server-side applications and APIs. I leverage the non-blocking, event-driven nature of Node.js to handle concurrent requests efficiently. I am well-versed in popular frameworks like Express.js, which enables rapid development and flexible routing",
+    },
+    {
+        name:"Python",
+        img:"icons8-python-240.png",
+        description:"As a Python developer, I possess a strong command of the language and its extensive ecosystem. I excel at leveraging Python's simplicity and readability to build robust and scalable applications."
+    },
+
+    {
+        name:"JAVA",
+        img:"icons8-java-240.png"
+    }
+]
