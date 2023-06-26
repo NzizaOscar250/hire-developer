5169973d1505bb5eef613f886d10271576e40dc1
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-06-25 21:39:56 -0700

    no amazing changes made

--- js/data.js
+++ js/data.js
@@ -17,6 +17,59 @@ const backend_data=[
 
     {
         name:"JAVA",
-        img:"icons8-java-240.png"
+        img:"icons8-java-240.png",
+        description: "As a Java developer, I have a deep understanding of the Java " +
+    "programming language and its ecosystem. I excel at developing " +
+    "robust and scalable applications using Java's " +
+    "object-oriented principles. With expertise in frameworks like Spring, " +
+    "I can efficiently build enterprise-level applications with features " +
+    "like dependency injection and MVC architecture."
+    },
+
+    {
+        name:"SQL",
+        img:"icons8-my-sql-240.png",
+        description:"As an SQL developer, I specialize in managing and querying relational databases. I possess expertise in SQL (Structured Query Language) and understand its syntax, constructs, and best practices. I can efficiently design and implement database schemas, ensuring proper normalization and data integrity"
+    },
+    {
+        name:"MongoDB",
+        img:"icons8-mongodb-240.png",
+        description:"As a MongoDB developer, I strive for excellence in my work. With a comprehensive understanding of MongoDB's document-oriented database model, I can design efficient and scalable data structures. I am adept at writing complex queries and aggregations, ensuring optimal performance and retrieval of data"
+    }
+];
+
+
+const front_end=[
+    {
+        name:"HTML-5",
+        img:"icons8-html5-240.png",
+        description:"As an HTML developer, I take pride in my ability to excel in my field. With a deep understanding of HTML, I can write clean and well-structured code. Attention to detail is a top priority for me, ensuring that every element and attribute is precisely implemented.",
+
+    },
+    {
+        name:"CSS-3",
+        img:"icons8-css-240.png",
+        description:" As a CSS developer, I specialize in styling and designing captivating web interfaces. I possess a deep understanding of CSS (Cascading Style Sheets) and its various properties, selectors, and layout techniques. I excel at creating visually appealing and responsive designs that enhance user experiences. With expertise in CSS frameworks like Bootstrap or Tailwind CSS, I can streamline development and ensure cross-browser compatibility"
+    },
+    {
+        name:"JAVASCRIPT",
+        img:"icons8-js-240.png",
+        description:`As a JavaScript developer, I specialize in building interactive websites. I have a strong understanding of JavaScript and use it to create dynamic and engaging user interfaces.  I also work with APIs and handle asynchronous operations for smooth data retrieval. With my skills in JavaScript, I can create both front-end and back-end applications, ensuring a seamless user experience across platforms`
+    },
+    {
+        name:"REACT JS",
+        img:"icons8-react-js-240.png",
+        description:`As a React.js developer, I focus on building robust and scalable web applications. I have expertise in utilizing React.js, a popular JavaScript library, to create dynamic and interactive user interfaces. I excel at developing reusable and modular components, ensuring efficient code organization and maintenance. 
+        Additionally, I have experience in integrating external APIs and libraries to enhance functionality. Overall, my React.js skills allow me to deliver powerful and responsive web applications.`
+    },
+    {
+        name:"TAILWIND CSS",
+        img:"icons8-tailwind-css-240.png",
+        description:`As a developer familiar with Tailwind CSS, I specialize in creating efficient and customizable web interfaces. Tailwind CSS is a utility-first CSS framework that allows for rapid development by providing a comprehensive set of pre-defined classes. With Tailwind CSS, I can easily create responsive layouts, apply responsive typography, and customize the visual appearance of elements`
+    },
+    {
+        name:`BOOTSTRAP-5`,
+        img:"icons8-bootstrap-240.png",
+        description:"As a developer experienced with Bootstrap, I specialize in creating responsive and visually appealing web interfaces. Bootstrap is a popular front-end framework that provides a collection of pre-built components, responsive grid system, and CSS styling."
     }
 ]
--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ const links= document.querySelector(".links");
 //const dropdown=document.querySelector("#dropit");
 const year=document.querySelector("#year");
 const date=new Date();
-
+const backend=document.querySelector("#backend");
+const frontend=document.querySelector("#frontend");
 const fullyear=date.getFullYear();
 
 
@@ -12,3 +13,50 @@ function dropdown(){
 
 // display the year automatically
 year.innerHTML=fullyear;
+
+
+
+function applyBackend(items){
+ items.map((data,key)=>{
+    backend.innerHTML +=`
+    <div class="flex flex-col sm:flex-row bg-white shadow-md border  rounded-md py-3">
+                 <div class="header w-96  sm:self-center px-2">
+                        <div class="card-image position-relative">
+                            <img src="img/${data.img}" class=" position-relative" alt="" srcset="">
+                        </div>
+                        <h4 class="title text-left sm:text-center text-medium text-indigo-500 font-bold">
+                            ${data.name}
+                        </h4>
+                 </div>
+                 <div class="body px-2 py-3 "><p>${data.description}</p></div>
+            </div>
+
+    `
+ })
+    
+
+}
+
+function applyFrontEnd(items){
+    items.map((data,key)=>{
+      frontend.innerHTML +=`
+       <div class="flex flex-col sm:flex-row bg-white shadow-md border  rounded-md py-3">
+                    <div class="header w-96  sm:self-center px-2">
+                           <div class="card-image position-relative">
+                               <img src="img/${data.img}" class=" position-relative" alt="" srcset="">
+                           </div>
+                           <h4 class="title text-left sm:text-center text-medium text-indigo-500 font-bold">
+                               ${data.name}
+                           </h4>
+                    </div>
+                    <div class="body px-2 py-3 "><p>${data.description}</p></div>
+               </div>
+   
+       `
+    })
+       
+   
+   }
+   
+   applyFrontEnd(front_end);
+applyBackend(backend_data);
